41987a629bf08a7d146a83a4d6abfb2771afb934
semen92 2git <74002782+semenovaa9205@users.noreply.github.com>
2022-12-07 16:35:58 +0300

    add kvadrat
    add figura kvadrt

---
+++ kvadrat.py
@@ -0,0 +1,9 @@
+import turtle
+window=turtle.Screen()
+window.title("turtles")
+turtle.shape("turtle")
+turtle.color("black")
+for i in range(4):
+    turtle.forward(100)
+    turtle.left(90)
+window.mainloop()
